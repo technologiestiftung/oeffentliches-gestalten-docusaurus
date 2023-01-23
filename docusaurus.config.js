73722b057fc23aa10ae7fedbf3e5996dbf87a629
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2023-01-23 17:24:13 +0100

    feat: provide footer links content

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,53 +1,53 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Öffentliches Gestalten',
-  tagline: 'Handbuch für innovatives Arbeiten in der Verwaltung',
-  baseUrl: '/',
-  url: 'https://oeffentliches-gestalten.org',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  title: "Öffentliches Gestalten",
+  tagline: "Handbuch für innovatives Arbeiten in der Verwaltung",
+  baseUrl: "/",
+  url: "https://oeffentliches-gestalten.org",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'CityLAB Berlin', // Usually your GitHub org/user name.
-  projectName: 'oeffentliches-gestalten', // Usually your repo name.
+  organizationName: "CityLAB Berlin", // Usually your GitHub org/user name.
+  projectName: "oeffentliches-gestalten", // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'de',
-    locales: ['de'],
+    defaultLocale: "de",
+    locales: ["de"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -57,72 +57,49 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'Öffentliches Gestalten',
+        title: "Öffentliches Gestalten",
         logo: {
-          alt: 'Icon zum Handbuch Öffentliches Gestalten',
-          src: 'img/logo.svg',
+          alt: "Icon zum Handbuch Öffentliches Gestalten",
+          src: "img/logo.svg",
         },
         items: [
           {
-            type: 'doc',
-            docId: 'vorwort',
-            position: 'left',
-            label: 'Handbuch',
+            type: "doc",
+            docId: "vorwort",
+            position: "left",
+            label: "Handbuch",
           },
-          {to: '/blog', label: 'Methodenbox', position: 'left'},
+          { to: "/blog", label: "Methodenbox", position: "left" },
           {
-            type: 'localeDropdown',
-            position: 'right',
+            type: "localeDropdown",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'light',
-        // links: [
-        //   {
-        //     title: 'Docs',
-        //     items: [
-        //       {
-        //         label: 'Tutorial',
-        //         to: '/docs/intro',
-        //       },
-        //     ],
-        //   },
-        //   {
-        //     title: 'Community',
-        //     items: [
-        //       {
-        //         label: 'Stack Overflow',
-        //         href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-        //       },
-        //       {
-        //         label: 'Discord',
-        //         href: 'https://discordapp.com/invite/docusaurus',
-        //       },
-        //       {
-        //         label: 'Twitter',
-        //         href: 'https://twitter.com/docusaurus',
-        //       },
-        //     ],
-        //   },
-        //   {
-        //     title: 'More',
-        //     items: [
-        //       {
-        //         label: 'Blog',
-        //         to: '/blog',
-        //       },
-        //       {
-        //         label: 'GitHub',
-        //         href: 'https://github.com/facebook/docusaurus',
-        //       },
-        //     ],
-        //   },
-        // ],
+        style: "light",
+        links: [
+          {
+            label: "Kontakt",
+            href: "https://citylab-berlin.org/de/projects/handbuch/",
+          },
+          {
+            label: "Quellcode",
+            href: "https://github.com/technologiestiftung/oeffentliches-gestalten-docusaurus",
+          },
+          {
+            label: "Datenschutz",
+            href: "https://www.technologiestiftung-berlin.de/datenschutz",
+          },
+          {
+            label: "Impressum",
+            href: "https://www.technologiestiftung-berlin.de/impressum",
+          },
+        ],
         copyright: `CC BY-NC-SA 4.0 ${new Date().getFullYear()} Technologiestiftung Berlin`,
       },
       colorMode: {
-        defaultMode: 'light',
+        defaultMode: "light",
         disableSwitch: true,
         respectPrefersColorScheme: true,
       },
