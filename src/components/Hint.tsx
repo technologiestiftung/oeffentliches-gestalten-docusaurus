44d277b4c15aa0e7673230b928ae48e478c22d9d
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2023-03-30 13:15:51 +0200

    fix: prevent layout jumps due to images loading

--- src/components/Hint.tsx
+++ src/components/Hint.tsx
@@ -9,7 +9,7 @@ interface HintProps {
 export const Hint: FC<HintProps> = ({ children }) => {
   return (
     <p className="flex gap-x-3 items-start md:-translate-x-2">
-      <WarningIcon className="shrink-0 w-6 h-auto overflow-visible translate-y-0.5" />
+      <WarningIcon className="shrink-0 w-6 aspect-square overflow-visible translate-y-0.5" />
       <span>{children}</span>
     </p>
   );
