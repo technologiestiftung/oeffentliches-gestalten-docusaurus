ca105de59072d35c1f23e3b0fee37318adfbbf4d
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2023-04-03 10:38:05 +0200

    chore: fields don't allow empty strings

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -130,9 +130,9 @@ const config = {
         respectPrefersColorScheme: false,
       },
       algolia: {
-        appId: process.env.ALGOLIA_APP_ID || "",
-        apiKey: process.env.ALGOLIA_API_KEY || "",
-        indexName: process.env.ALGOLIA_SEARCH_INDEX || "",
+        appId: process.env.ALGOLIA_APP_ID || "123",
+        apiKey: process.env.ALGOLIA_API_KEY || "123",
+        indexName: process.env.ALGOLIA_SEARCH_INDEX || "index",
       },
     }),
   customFields: {
