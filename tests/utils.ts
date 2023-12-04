4d08ce82116017a684b5b3a01f940a4783f3cdee
Raphael Arce <raphael.arce@ts.berlin>
2023-12-04 13:22:38 +0100

    feat: added visual regression tests
    Signed-off-by: Raphael Arce <raphael.arce@ts.berlin>

---
+++ tests/utils.ts
@@ -0,0 +1,14 @@
+import * as cheerio from 'cheerio';
+import * as fs from 'fs';
+
+// Extract a list of pathnames, given a fs path to a sitemap.xml file
+// Docusaurus generates a build/sitemap.xml file for you!
+export function extractSitemapPathnames(sitemapPath: string): string[] {
+  const sitemap = fs.readFileSync(sitemapPath).toString();
+  const $ = cheerio.load(sitemap, {xmlMode: true});
+  const urls: string[] = [];
+  $('loc').each(function handleLoc() {
+    urls.push($(this).text());
+  });
+  return urls.map((url) => new URL(url).pathname);
+}
